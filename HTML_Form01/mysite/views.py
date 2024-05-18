7aaf774c2823b7c68c3bac75ea4ea7149b8b1024
Andy Kuo <b11902171@csie.ntu.edu.tw>
2024-05-18 19:10:47 +0800

    form get

--- HTML_Form01/mysite/views.py
+++ HTML_Form01/mysite/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def get_example(request):
+    try:
+        urid= request.GET['user_id']
+        urpass= request.GET['user_pass']
+        se_byear = request.GET['byear']
+        urfcolor = request.GET.getlist('fcolor')
+    except:
+        urid= None
+    
+    if urid != None and urpass == '12345':
+        verified = True
+    else:
+        verifeid= False
+        
+    years = range(1960, 2022 + 1)
+        
+    return render(request, 'get_example.html', locals())
